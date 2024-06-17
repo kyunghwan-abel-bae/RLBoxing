f318c76f98687c903d6acabc72f54a08a7a0f748
Kyunghwan Abel Bae <kyunghwan.abel.bae@gmail.com>
2024-06-17 21:43:54 +0900

    roughly running, save function not working

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-import os
-
 import datetime
 from pathlib import Path
 
 import gymnasium as gym
+import numpy as np
 from gym.spaces import Box
 from gym.wrappers import FrameStack, GrayScaleObservation, TransformObservation
 
 from a2cagent import A2CAgent
 from metrics import MetricLogger
 from wrappers import ResizeObservation, AdapterGrayScaleObservation
-
-import numpy as np
 
 env = gym.make('BoxingDeterministic-v4', render_mode="rgb_array")
 # env = gym.make('BoxingDeterministic-v4', render_mode="human")
@@ -42,21 +39,28 @@ for e in range(episodes):
     state = env.reset()
     total_reward = 0
 
+    actor_loss, critic_loss = 0
+
     while True:
-        print(f"state : {state.shape}")
         action = agent.act(state)
-        print(f"action : {action}")
-        quit()
+
+        # print(f"state : {state}")
 
         next_state, reward, done, info = env.step(action)
-
+        # next_state = np.array(next_state)
+        # print(f"next_state : {next_state}, reward : {reward}, done : {done}")
+        # print("point1")
         total_reward += reward if reward > 0 else 0
 
-        q, loss = agent.learn()
+        q, loss = agent.learn(state, action, reward, next_state, done)
+        # print("point2")
+        # quit()
 
         logger.log_step(total_reward, loss, q)
 
         state = next_state
+
+        # print("point5")
 
         if done or (total_reward > 99):
             break
@@ -65,13 +69,12 @@ for e in range(episodes):
 
     if total_reward > 99:
         print("KNOCK OUT")
-        logger.record(
-            episode=e,
-            epsilon=agent.exploration_rate,
-            step=agent.curr_step
-        )
 
-        agent.save()
+        #     def write_summary(self, score, actor_loss, critic_loss, step):
+        agent.write_summary(total_reward)
+
+        agent.save_model()
+
         break
 
     if e % 50 == 0:
